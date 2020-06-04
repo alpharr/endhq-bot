e425d170ba90e7945818fc0bbfd0820accc09073
alpharr <tcmpvh8@gmail.com>
2020-06-04 10:11:49 -0400

    agdfsdfg

--- index.js
+++ index.js
@@ -45,4 +45,23 @@ bot.on('message', function(message){
     }
 })
 
+Discord.Client instance here.on("message", message => {
+    if(message.author.bot) return;
+
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(message.channel.type === "dm") return;
+
+    if(!message.content.startsWith('+')) return;
+
+    if(command === '+ann') {
+        let channel = message.mentions.channels();
+        let announcement = args.slice(1).join(" ");
+
+        channel.send(announcement);
+    }
+}
+
 bot.login(process.env.token);
